235124487191e396d1bab9b54bdf33a914cdfd92
MinhQuyennn <20521824@gm.uit.edu.vn>
2024-04-05 00:38:02 +0700

    fix

--- frontend/src/Components/Attendance.js
+++ frontend/src/Components/Attendance.js
@@ -6,7 +6,24 @@ const Attendance = () => {
     <div className="home-container">
       <Navbar />
       <div className="home-banner-container">
-        hiii
+      <h1>Attendance</h1>
+
+    <table>
+        <tr id="header">
+            <th>No</th>
+            <th>Name</th>
+            <th>Time</th>
+            <th>Image</th>
+        </tr>
+        <tr>
+            <td>1 </td>
+            <td>sasa </td>
+            <td>10pm </td>
+            <td>USA </td>
+
+        </tr>
+
+    </table>
       </div>
     </div>
   );
