acd0906912bf1625441ee05b5d23273b78e765c3
MinhQuyennn <20521824@gm.uit.edu.vn>
2024-04-14 14:53:18 +0700

    update

--- frontend/src/Components/Attendance.js
+++ frontend/src/Components/Attendance.js
@@ -10,25 +10,28 @@ const Attendance = () => {
   const [selectedOption, setSelectedOption] = useState('Attendance'); // Default to 'Attendance'
 
   useEffect(() => {
-    async function fetchAllAttendance() {
-      try {
-        const response = await fetch("http://localhost:8080/");
-        if (!response.ok) {
-          throw new Error("Failed to fetch attendance data");
-        }
-        const data = await response.json();
-        // Sort the attendance data based on time in descending order
-        data.sort((a, b) => {
-          return new Date(b.time.replace('_', ' ')) - new Date(a.time.replace('_', ' '));
-        });
-        setAttendanceData(data);
-      } catch (error) {
-        console.error("Error fetching attendance data:", error.message);
-      }
-    }
+    fetchAttendanceData(); // Fetch data initially
+    const interval = setInterval(fetchAttendanceData, 5000); // Poll every 5 seconds
 
-    fetchAllAttendance();
+    return () => clearInterval(interval); // Cleanup interval on unmount
   }, []);
+
+  const fetchAttendanceData = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/");
+      if (!response.ok) {
+        throw new Error("Failed to fetch attendance data");
+      }
+      const data = await response.json();
+      // Sort the attendance data based on time in descending order
+      data.sort((a, b) => {
+        return new Date(b.time.replace('_', ' ')) - new Date(a.time.replace('_', ' '));
+      });
+      setAttendanceData(data);
+    } catch (error) {
+      console.error("Error fetching attendance data:", error.message);
+    }
+  };
 
   const openImage = (image) => {
     setSelectedImage(image);
@@ -76,29 +79,27 @@ const Attendance = () => {
               <tr id="header">
                 <th>Time</th>
                 <th>Name</th>
-                
                 <th>Image</th>
-                
               </tr>
               {filteredAttendance.map((attendance) => (
-                <tr key={attendance._id}>
-                  <td>{attendance.time}</td>
-                  <td>{attendance.name}</td>
-                  
-                  <td>
-                    {/* Display the image if available */}
-                    {attendance.image && (
-                      <img
-                        src={`data:image/jpeg;base64,${encodeBase64FromArrayBuffer(
-                          attendance.image.data
-                        )}`}
-                        alt="Attendance Image"
-                        style={{ maxWidth: "100px", cursor: "pointer" }}
-                        onClick={() => openImage(attendance.image)}
-                      />
-                    )}
-                  </td>
-                </tr>
+                <tr key={attendance._id} style={{ backgroundColor: attendance.name === 'intruder' ? '#4a63ee82' : 'inherit' }}>
+                <td>{attendance.time}</td>
+                <td>{attendance.name}</td>
+                <td>
+                  {/* Display the image if available */}
+                  {attendance.image && (
+                    <img
+                      src={`data:image/jpeg;base64,${encodeBase64FromArrayBuffer(
+                        attendance.image.data
+                      )}`}
+                      alt="Attendance Image"
+                      style={{ maxWidth: "100px", cursor: "pointer" }}
+                      onClick={() => openImage(attendance.image)}
+                    />
+                  )}
+                </td>
+              </tr>
+              
               ))}
             </tbody>
           </table>
